7c9d1b2041852c1761d21b62c865e7836547ec5f
Karsten <Karsten@WIN-163AD745QDH>
2024-10-08 14:02:55 -0700

    Add project files.

---
+++ kabeljongen_schakelmeisje/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace kabeljongen_schakelmeisje
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
