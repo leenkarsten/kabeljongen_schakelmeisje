2e5252f5cabd8c2997e112b41ca9bae2defbf79e
sandje05 <sanderbroekhof05@outlook.com>
2024-10-10 10:38:53 +0200

    created 2dMovementService

---
+++ kabeljongen_schakelmeisje/windows/MovementTest.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kabeljongen_schakelmeisje.windows
+{
+    /// <summary>
+    /// Interaction logic for MovementTest.xaml
+    /// </summary>
+    public partial class MovementTest : Window
+    {   
+        private MovementService movementService;
+        public MovementTest()
+        {
+            InitializeComponent();
+
+            movementService = new MovementService(Player, Player2, this);
+        }
+    }
+}
