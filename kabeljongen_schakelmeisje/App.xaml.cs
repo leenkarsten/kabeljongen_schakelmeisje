5f748ab551c854e36d6aa574d1d27d69e627f8e7
leenkarsten <karsten.hoekstra1@student.nhlstenden.com>
2024-10-10 12:57:27 -0700

    added sound, audio settings overlay,

--- kabeljongen_schakelmeisje/App.xaml.cs
+++ kabeljongen_schakelmeisje/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Media;
 
 namespace kabeljongen_schakelmeisje
 {
@@ -8,7 +9,24 @@ namespace kabeljongen_schakelmeisje
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
+
     {
+        public static MediaPlayer mediaPlayer = new MediaPlayer();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            mediaPlayer.Open (new System.Uri("C:\\Users\\Karsten\\Downloads\\game-music-loop-6-144641.mp3"));
+
+            mediaPlayer.Volume = 0.5;
+
+            mediaPlayer.MediaEnded += (s, ev) => mediaPlayer.Position = TimeSpan.Zero;
+
+            mediaPlayer.Play();
+        }
     }
+
+
 
 }
