02dd89c43f1002f40221de902ac4d030b5fce9a8
Rick Wassink <rick.wassink@student.nhlstenden.com>
2024-10-17 12:03:37 +0200

    werkende knop

--- kabeljongen_schakelmeisje/windows/MovementTest.xaml.cs
+++ kabeljongen_schakelmeisje/windows/MovementTest.xaml.cs
@@ -1,13 +1,13 @@
 ﻿using kabeljongen_schakelmeisje.Services;
 using System.ComponentModel;
-using System.Numerics;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace kabeljongen_schakelmeisje.windows
 {
-    /// <summary>
-    /// Interaction logic for MovementTest.xaml
-    /// </summary>
     public partial class MovementTest : Window, INotifyPropertyChanged
     {
         private double _groundHeight;
@@ -24,14 +24,8 @@ namespace kabeljongen_schakelmeisje.windows
         public event PropertyChangedEventHandler PropertyChanged;
 
         private CollisionDetectionService collisionService;
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
         private MovementService movementService;
-
+        private bool batteryActivated = true; 
         public MovementTest()
         {
             InitializeComponent();
@@ -42,18 +36,39 @@ namespace kabeljongen_schakelmeisje.windows
             CheckCollisons(Player, box, OnCollisionDetected);
 
             double screenHeight = SystemParameters.PrimaryScreenHeight;
-            GroundHeight = screenHeight - 50; // Set the ground height based on the window height
+            GroundHeight = screenHeight - 28; // Set the ground height based on the window height
         }
 
         private void CheckCollisons(UIElement obj1, UIElement obj2, System.Action method)
         {
             collisionService = new CollisionDetectionService(obj1, obj2);
-            collisionService.CollisionDetected += OnCollisionDetected;
+            collisionService.CollisionDetected += method;
         }
-
+        
         private void OnCollisionDetected()
         {
-            MessageBox.Show("This is a basic alert message.", "Alert");
+            if (batteryActivated)
+            {
+                MessageBox.Show("Battery action triggered!", "Action");
+                batteryActivated = false;
+            }  
+        }
+
+        // Handle key events
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.E)
+            {
+                if(batteryActivated)
+                { 
+                    CheckCollisons(Player, Battery, OnCollisionDetected); 
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
