ff34f1abcfd9a31f4f01e13fc373553d7653c962
sandje05 <sanderbroekhof05@outlook.com>
2024-10-17 10:39:41 +0200

    collision detection

--- kabeljongen_schakelmeisje/windows/MovementTest.xaml.cs
+++ kabeljongen_schakelmeisje/windows/MovementTest.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using kabeljongen_schakelmeisje.Services;
+using System.ComponentModel;
+using System.Numerics;
 using System.Windows;
 
 namespace kabeljongen_schakelmeisje.windows
@@ -21,6 +23,8 @@ namespace kabeljongen_schakelmeisje.windows
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private CollisionDetectionService collisionService;
+
         protected void OnPropertyChanged(string name)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
@@ -35,8 +39,21 @@ namespace kabeljongen_schakelmeisje.windows
 
             movementService = new MovementService(Player, Player2, this, Ground);
 
+            CheckCollisons(Player, box, OnCollisionDetected);
+
             double screenHeight = SystemParameters.PrimaryScreenHeight;
             GroundHeight = screenHeight - 50; // Set the ground height based on the window height
+        }
+
+        private void CheckCollisons(UIElement obj1, UIElement obj2, System.Action method)
+        {
+            collisionService = new CollisionDetectionService(obj1, obj2);
+            collisionService.CollisionDetected += OnCollisionDetected;
+        }
+
+        private void OnCollisionDetected()
+        {
+            MessageBox.Show("This is a basic alert message.", "Alert");
         }
     }
 }
